85cbb10132b01a13630e7d35c41307c0f1d8304b
Michael Pace <terinopace@gmail.com>
2023-04-23 21:53:35 -0500

    Add project

---
+++ Scaffold/ScaffoldApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScaffoldApp.swift
+//  Scaffold
+//
+//  Created by Michael Pace on 4/23/23.
+//
+
+import SwiftUI
+
+@main
+struct ScaffoldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
